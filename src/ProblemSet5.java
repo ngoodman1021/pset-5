13ce60c72704d23f50072bb96648b55741007204
Naya-Jevan Goodman <ngoodman@ucvts.org>
2019-11-04 14:00:34 -0500

    third commit

--- src/ProblemSet5.java
+++ src/ProblemSet5.java
@@ -31,7 +31,15 @@ public class ProblemSet5 {
 	    
        System.out.println("\nExercise 1: " + ps.surroundMe("naya", "playa"));
        System.out.println("\nExercise 2: " + ps.endsMeet("ultima", 2));
-		
+	   System.out.println("\nExercise 3: " + ps.middleMan("ImACatPerson"));
+       //System.out.println("\nExercise 4: " + ps.isCentered("ultima", 2));
+	   //System.out.println("\nExercise 5: " + ps.countMe("naya", "playa"));
+       //System.out.println("\nExercise 6: " + ps.triplets("ultima", 2));
+	   //System.out.println("\nExercise 7: " + ps.addMe("naya", "playa"));
+       //System.out.println("\nExercise 8: " + ps.sequence("ultima", 2));
+	   //System.out.println("\nExercise 9: " + ps.intertwine("naya", "playa"));
+       //System.out.println("\nExercise 10: " + ps.isPalindrome("ultima", 2));
+	   
     }
     
     /*
@@ -65,7 +73,7 @@ public class ProblemSet5 {
      */
     
     public String endsMeet(String text, int n) {
-	if(text == null || text.length() >= 10 || text.length() =< 1 || n =< 1 || n >= text.length()){
+	if(text == null || text.length() > 10 || text.length() < 1 || n < 1 || n > text.length()){
         	return text;
       	}else{
         	String first = text.substring(0, n);
@@ -74,86 +82,23 @@ public class ProblemSet5 {
         	return newString;
       }
     }
-    
-    /*
+	
+	/*
      * Exercise 3.
      * 
      * Given a string, return a new string using the middle three characters of text.
      */
     
     public String middleMan(String text) {
-
-    }
-    
-    /*
-     * Exercise 4.
-     * 
-     * Given two strings, determine whether or not target is equivalent to the middle
-     * three characters of text.
-     */
-    
-    public boolean isCentered(String text, String target) {
-
-    }
-    
-    /*
-     * Exercise 5.
-     * 
-     * Given a string and a character, compute the number of words that end in suffix.
-     */
-    
-    public int countMe(String text, char suffix) {
-
-    }
-    
-    /*
-     * Exercise 6.
-     * 
-     * Given a string, compute the number of triplets in text.
-     */
-    
-    public int triplets(String text) {
-
-    }
-    
-    /*
-     * Exercise 7.
-     * 
-     * Given a string, compute the sum of the digits in text.
-     */
-    
-    public long addMe(String text) {
-
-    }
-    
-    /*
-     * Exercise 8.
-     * 
-     * Given a string, compute the length of the longest sequence.
-     */
-    
-    public long sequence(String text) {
-
-    }
-    
-    /*
-     * Exercise 9.
-     * 
-     * Given two strings, return a new string built by intertwining each of the
-     * characters of a and b.
-     */
-    
-    public String intertwine(String a, String b) {
-
-    }
-    
-    /*
-     * Exercise 10.
-     * 
-     * Given a string, determine whether or not it is a palindrome.
-     */
-    
-    public boolean isPalindrome(String text) {
-
+		if(text == null || text.length() <3 || text.length() % 2 == 0){
+			return text;
+		}else{
+			int midNumber = text.length() / 2;
+			char first = text.charAt(midNumber - 1);
+			char second = text.charAt(midNumber);
+			char third = text.charAt(midNumber + 1);
+			String newChar = String.valueOf(first) + String.valueOf(second) + String.valueOf(third);
+			return newChar;
+		}
     }
 }
